d003eef646e8c9f294aa818a968a2d7225a0c54e
Lewis Dale <lewis.dale@fortacode.co.uk>
2022-11-14 10:35:10 +0000

    Fix export redirect for site model

--- src/site/mod.ts
+++ src/site/mod.ts
@@ -1,7 +1,7 @@
 import Gateway from "./gateway.ts";
-import { type SiteDetails as Model } from "./models.ts";
+import { type SiteDetails } from "./models.ts";
 
+export type Model = SiteDetails;
 export {
 	Gateway,
-	Model,
 };
